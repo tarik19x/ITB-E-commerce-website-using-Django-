8d58685f4541113d24393f0f7c2272f0fe205c6c
Asus <tarikhasan12200@gmail.com>
2020-04-14 17:11:51 +0600

    after creating superuser

--- shop/models.py
+++ shop/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Product(models.Model):
     Product_id=models.AutoField
     Product_name=models.CharField(max_length=100)
+    price= models.DecimalField(max_digits=12, decimal_places=0)
     desc=models.CharField(max_length=300)
     publish_date=models.DateField()
