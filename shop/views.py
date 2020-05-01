ddf3bb9f73f2c508547faa6f4074ade7dc009907
Asus <tarikhasan12200@gmail.com>
2020-05-02 03:01:57 +0600

    accoding to codewithharry 36 to 38

--- shop/views.py
+++ shop/views.py
@@ -7,13 +7,38 @@ import math
 # Create your views here.
 
 def index(request):
-    product_list=Product.objects.all()
+    # product_list=Product.objects.all()
     # print(len(product_list))
-    n=len(product_list)
-    no_of_slide=n//4 +math.ceil(n/4-n//4)
-    print(no_of_slide)
+    
+    allproducts=[]
+    
+    catprods=Product.objects.values('category','id')
+    # databaser er sob gula row er id+ category chole asbe bt oikhabe obossi kichu onek dupliacate value thakbe oigula bad niye just unique category k seter maddhome bachai kora
+    cats={item['category'] for item in catprods }
+    # print("catprods----->",catprods)
+    # print("cats---->>",cats)
+    
+    
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        # print("###--->",prod)
+        n=len(prod)
+        no_of_slide=n//4 +math.ceil(n/4-n//4)
+        
+        allproducts.append([prod,range(1,no_of_slide),no_of_slide])
+        
+        
+        
+        
+        
+    
+    # print("catprods----->",catprods)
+    # print("cat---->>",cats)
+     
 
-    allproducts=[[product_list,range(1,no_of_slide),no_of_slide],[product_list,range(1,no_of_slide),no_of_slide]]
+
+
+    # allproducts=[[product_list,range(1,no_of_slide),no_of_slide],[product_list,range(1,no_of_slide),no_of_slide]]
     product_group={'allproducts':allproducts}
 
     return render(request,"shop/index.html",product_group)
