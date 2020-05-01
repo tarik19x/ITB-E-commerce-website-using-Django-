d8c613ba4b18af38367b41323000c0e3e9e64475
Asus <tarikhasan12200@gmail.com>
2020-05-02 00:10:57 +0600

    adding html,css,database

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 class Product(models.Model):
     Product_id=models.AutoField
-    Product_name=models.CharField(max_length=100)
+    Product_name = models.CharField(max_length=100)
     price= models.DecimalField(max_digits=12, decimal_places=0)
     desc=models.CharField(max_length=300)
     publish_date=models.DateField()
+    category= models.CharField( max_length=50,default="")
+    subcategory = models.CharField( max_length=50,default="")
+    image = models.ImageField(upload_to="shop/images",default="")
+   
+    def __str__(self):
+        return self.Product_name
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+import math
 
 
 # Create your views here.
 
 def index(request):
-    return render(request,"shop/index.html")
+    product_list=Product.objects.all()
+    # print(len(product_list))
+    n=len(product_list)
+    no_of_slide=n//4 +math.ceil(n/4-n//4)
+    print(no_of_slide)
+
+    allproducts=[[product_list,range(1,no_of_slide),no_of_slide],[product_list,range(1,no_of_slide),no_of_slide]]
+    product_group={'allproducts':allproducts}
+
+    return render(request,"shop/index.html",product_group)
 
 
 def about(request):
-    return render(request,"shop/index.html")    
+    return render(request,"shop/about.html")    
 
 def contact(request):
     return render(request,"shop/index.html")
